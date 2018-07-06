8b47c776523c8d4c94c651cb95a7b10d9e82056a
pjialin <admin@pjialin.com>
2018-07-06 16:24:02 +0800

    配置小程序支持 Sass

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,6 +49,14 @@ module.exports = {
   },
   module: {
     rules: [
+        {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS
+            ]
+        },
       {
         test: /\.vue$/,
         loader: 'mpvue-loader',
